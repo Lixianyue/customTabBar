6c306ab440f38545cff9f0734e94a63a3bf5d42b
msd <msd@UserdeMacBook-Pro.local>
2014-12-01 09:05:22 +0800

    Initial Commit

---
+++ CustomTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTabBar
+//
+//  Created by msd on 14-12-1.
+//  Copyright (c) 2014年 msd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomTabBar
+//
+//  Created by msd on 14-12-1.
+//  Copyright (c) 2014年 msd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
